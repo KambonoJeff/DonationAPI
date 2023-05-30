62f1c53506d61d808ebc0ebb1bad88d4e0582357
KAMBONO <kambonoj@gmail.com>
2023-05-30 08:14:36 +0300

    Modified the FoodBank controller. Disabled the Delete functionality for the food records to ensure all records on donations cannot be deleted

--- donation_api/app/Http/Controllers/FoodBankController.php
+++ donation_api/app/Http/Controllers/FoodBankController.php
@@ -23,13 +23,13 @@ class FoodBankController extends Controller
         $sumsnacks = FoodBank::pluck('snacks')->toArray();
         $sumBreak = FoodBank::pluck('breakfast')->toArray();
 
-        //  $totalCash = array_sum($sumcash);
-        //  $totalBreak = array_sum($sumBreak);
-        //  $totalSnacks = array_sum($sumsnacks);
-        //  $totaproteins = array_sum($sumproteins);
-        //  $totalcereals = array_sum($sumcereals);
-        //  $totallegume = array_sum($sumlegume);
-        //  $data = [$totalcereals,$totaproteins,$totallegume,$totalBreak,$totalSnacks,$totalCash,$food];
+          $totalCash = array_sum($sumcash);
+          $totalBreak = array_sum($sumBreak);
+          $totalSnacks = array_sum($sumsnacks);
+          $totaproteins = array_sum($sumproteins);
+          $totalcereals = array_sum($sumcereals);
+          $totallegume = array_sum($sumlegume);
+          $data = [$totalcereals,$totaproteins,$totallegume,$totalBreak,$totalSnacks,$totalCash,$food];
 
         $data = $food;
 
@@ -87,13 +87,4 @@ class FoodBankController extends Controller
         ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        $data= FoodBank::findOrFail($id);
-        $data->delete();
-        return response(['deleted'],204);
-    }
 }

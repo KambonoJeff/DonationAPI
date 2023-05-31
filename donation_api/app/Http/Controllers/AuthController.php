b12e66222d2da4a44d2903c609acdec2ba899ec4
KAMBONO <kambonoj@gmail.com>
2023-05-31 21:56:30 +0300

    modified

--- donation_api/app/Http/Controllers/AuthController.php
+++ donation_api/app/Http/Controllers/AuthController.php
@@ -27,11 +27,10 @@ class AuthController extends Controller
       }
       $user = User::where('email', $request->email)->first();
 
-      return response()->json(['data'=>'skdfvskdfhbvskdjhfbv']);
-      // return response()->json([
-      //   'user'=>$user,
-      //   'token'=>$user->createToken('Api token of'.$user->name)->plainTextToken
-      // ]);
+       return response()->json([
+         'user'=>$user,
+         'token'=>$user->createToken('Api token of'.$user->name)->plainTextToken
+       ]);
 
     }
 

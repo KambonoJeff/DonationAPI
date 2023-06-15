7d0f470a602f055ab32cf7319a4aeaf3ea796e10
KAMBONO <kambonoj@gmail.com>
2023-06-15 22:45:42 +0300

    Debug

--- donation_api/app/Http/Requests/StoreNgoRequest.php
+++ donation_api/app/Http/Requests/StoreNgoRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreNgoRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * 
      */
     public function authorize(): bool
     {
